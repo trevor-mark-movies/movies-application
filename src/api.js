8d59333c060c943846d528181ac796f64f0e6ef4
trevormullis12 <trevor.mullis12@gmail.com>
2020-05-28 11:28:11 -0500

    added html and partial post functionality

--- src/api.js
+++ src/api.js
@@ -2,5 +2,20 @@ module.exports = {
   getMovies: () => {
     return fetch('/api/movies')
       .then(response => response.json());
+  },
+
+  postMovie: (title, rating) => {
+    const moviePost = {title, rating};
+    const url = 'api/movies';
+    const options = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(moviePost),
+    };
+    fetch(url, options)
+        .then(console.log('success'))
+        .catch(error => console.log(error));
   }
 };
--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+const $ = require('jquery');
 /**
  * es6 modules and imports
  */
@@ -7,14 +8,21 @@ sayHello('World');
 /**
  * require style imports
  */
-const {getMovies} = require('./api.js');
+const {getMovies, postMovie} = require('./api.js');
 
 getMovies().then((movies) => {
   console.log('Here are all the movies:');
+  $('.container').html("");
   movies.forEach(({title, rating, id}) => {
     console.log(`id#${id} - ${title} - rating: ${rating}`);
+    $('.container').append(`<h1>id#${id} - ${title} - rating: ${rating}</h1>`);
   });
 }).catch((error) => {
   alert('Oh no! Something went wrong.\nCheck the console for details.')
   console.log(error);
 });
+
+
+$('#post-movie').click(
+  postMovie($('#movie-title-submit'), 5)
+)
